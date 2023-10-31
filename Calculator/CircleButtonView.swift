121859ea8d9f17de4e339f041e9555cc304c44d9
Nina Zhou <nzhou@lcs.on.ca>
2023-10-31 14:45:38 -0400

    circle buttons

---
+++ Calculator/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  Calculator
+//
+//  Created by Shuyu Zhou on 2023-10-31.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
