e437c6e62739d6e8184d336a8b19032902d119c8
Nina Zhou <nzhou@lcs.on.ca>
2023-10-31 15:19:16 -0400

    color of the circles

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -16,39 +16,40 @@ struct ContentView: View {
                 .foregroundStyle(.white)
                 .padding(.leading, 100)
             HStack{
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "C", colorOfTheNumber: .black)
+                CircleButtonView(circleColor:.gray, numberOnTheCircle: "C", colorOfTheNumber: .black)
                 CircleButtonView(circleColor: .gray, numberOnTheCircle: "+/-", colorOfTheNumber: .black)
                 CircleButtonView(circleColor: .gray, numberOnTheCircle: "%", colorOfTheNumber: .black)
                 CircleButtonView(circleColor: .orange, numberOnTheCircle: "/", colorOfTheNumber: .white)
             }
             HStack{
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "7", colorOfTheNumber: .white)
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "8", colorOfTheNumber: .white)
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "9", colorOfTheNumber: .white)
+                CircleButtonView(circleColor: Color("Dark Grey"), numberOnTheCircle: "7", colorOfTheNumber: .white)
+                CircleButtonView(circleColor: Color("Dark Grey"), numberOnTheCircle: "8", colorOfTheNumber: .white)
+                CircleButtonView(circleColor: Color("Dark Grey"), numberOnTheCircle: "9", colorOfTheNumber: .white)
                 CircleButtonView(circleColor: .orange, numberOnTheCircle: "*", colorOfTheNumber: .white)
             }
             HStack{
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "4", colorOfTheNumber: .white)
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "5", colorOfTheNumber: .white)
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "6", colorOfTheNumber: .white)
+                CircleButtonView(circleColor: Color("Dark Grey"), numberOnTheCircle: "4", colorOfTheNumber: .white)
+                CircleButtonView(circleColor:Color("Dark Grey"), numberOnTheCircle: "5", colorOfTheNumber: .white)
+                CircleButtonView(circleColor:Color("Dark Grey"), numberOnTheCircle: "6", colorOfTheNumber: .white)
                 CircleButtonView(circleColor: .orange, numberOnTheCircle: "-", colorOfTheNumber: .white)
             }
             HStack{
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "1", colorOfTheNumber: .white)
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "2", colorOfTheNumber: .white)
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: "3", colorOfTheNumber: .white)
+                CircleButtonView(circleColor: Color("Dark Grey"), numberOnTheCircle: "1", colorOfTheNumber: .white)
+                CircleButtonView(circleColor:Color("Dark Grey"), numberOnTheCircle: "2", colorOfTheNumber: .white)
+                CircleButtonView(circleColor: Color("Dark Grey"), numberOnTheCircle: "3", colorOfTheNumber: .white)
                 CircleButtonView(circleColor: .orange, numberOnTheCircle: "+", colorOfTheNumber: .white)
             }
             HStack{
                 ZStack{
                     Capsule()
                         .frame(width: 175, height: 80)
-                        .foregroundColor(.gray)
+                        .foregroundColor(Color("Dark Grey"))
                     Text("0")
                         .font(Font.system(size: 40))
                         .frame(alignment: .leading)
+                        .padding(.trailing, 80)
                 }
-                CircleButtonView(circleColor: .gray, numberOnTheCircle: ".", colorOfTheNumber: .white)
+                CircleButtonView(circleColor: Color("Dark Grey"), numberOnTheCircle: ".", colorOfTheNumber: .white)
                 CircleButtonView(circleColor: .orange, numberOnTheCircle: "=", colorOfTheNumber: .white)
             }
             .padding(.bottom)
