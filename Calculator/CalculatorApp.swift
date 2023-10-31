82e26c68a20ebeea7207b5da929de936f2ff31db
Nina Zhou <nzhou@lcs.on.ca>
2023-10-31 14:35:14 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Shuyu Zhou on 2023-10-31.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
